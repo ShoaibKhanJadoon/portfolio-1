a36118a1d80c5afb89f315b5bea921b7db2e1e4f
ShoaibKhanJadoon <140436897+ShoaibKhanJadoon@users.noreply.github.com>
2025-03-22 12:42:14 +0500

    Update Footer.tsx
    (report a bug) button appears on top of social media links. user won't be able to click. now fixed.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex mt-16 md:flex-row flex-col justify-around items-center gap-4 md:gap-0">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Adrian Hajdin
         </p>
